d1ebe36c4550b1ddfeba06b4cc8da68274f1649f
AndyNguyen002 <ngocdungokla@gmail.com>
2022-09-18 14:28:34 +0700

    app login

--- App.js
+++ App.js
@@ -1,20 +1,64 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput,Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TextInput
+          style={styles.input}
+          placeholder='Email'
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Password"
+        />
+        
+        <View style={styles.button}>
+          <Button
+            title="Login"
+            color="#00b6ed"
+            borderWidth='0'
+          />
+        </View> 
+        
+          <Text style={styles.textstyle}>
+            Forgot your password?
+          </Text>
+
+          <Text style={styles.textstyle}>
+            Register
+          </Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  container:{
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent:"center",
+    alignItems:"center",
+    backgroundColor:"#dddddd",
+    flexDirection:'column',
+    
   },
+  input: {
+    height: 40,
+    width:'70%',
+    margin: 12,
+    borderWidth: 0,
+    paddingLeft:50,
+    borderRadius:20,
+    backgroundColor:'white'
+  },
+  button:{
+    width: '65%',
+    padding:5,
+    backgroundColor:'#00b6ed ',
+    borderRadius:20, 
+    marginTop:12
+  },
+  textstyle:{
+    fontSize:12,
+    marginTop: 30,
+  }
 });
